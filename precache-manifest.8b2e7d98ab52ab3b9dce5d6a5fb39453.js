76b4ae7373105a4d0136c60935165e284c455e17
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-06-07 14:26:46 +0300

    Updates

---
+++ precache-manifest.8b2e7d98ab52ab3b9dce5d6a5fb39453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add8102bfe4c9d29e5e1b7179fb1d4f1",
+    "url": "/kalashnykbohdan/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "cd818abb9c5edc5db032",
+    "url": "/kalashnykbohdan/goit-react-hw-01-components/static/css/main.ef8fef7f.chunk.css"
+  },
+  {
+    "revision": "57cc350bedf9d15a6de4",
+    "url": "/kalashnykbohdan/goit-react-hw-01-components/static/js/2.835e3c52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kalashnykbohdan/goit-react-hw-01-components/static/js/2.835e3c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd818abb9c5edc5db032",
+    "url": "/kalashnykbohdan/goit-react-hw-01-components/static/js/main.c7f2a322.chunk.js"
+  },
+  {
+    "revision": "3dcb37d3ab218761606e",
+    "url": "/kalashnykbohdan/goit-react-hw-01-components/static/js/runtime-main.a71fe7fc.js"
+  }
+]);
